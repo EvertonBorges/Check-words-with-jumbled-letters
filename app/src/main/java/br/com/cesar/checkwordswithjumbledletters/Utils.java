be13569f824dc63d86f12efc1f99fb3a518c9129
EvertonBorges <evertonsoaresborges@gmail.com>
2020-05-22 15:32:02 -0400

    fix - improved partial permutation check

--- app/src/main/java/br/com/cesar/checkwordswithjumbledletters/Utils.java
+++ app/src/main/java/br/com/cesar/checkwordswithjumbledletters/Utils.java
@@ -6,7 +6,16 @@ class Utils {
         if (firstWord == null || secondWord == null) return false;
         if (firstWord.charAt(0) != secondWord.charAt(0)) return false;
         if (firstWord.length() != secondWord.length()) return false;
-        if (firstWord.length() <= 3) return true;
+
+        boolean hasSameLetters = checkEachLetter(firstWord, secondWord);
+        if (!hasSameLetters) return false;
+
+        if (firstWord.length() > 3) return checkByNumberPartialPermutation(firstWord, secondWord);
+
+        return true;
+    }
+
+    private static boolean checkByNumberPartialPermutation(String firstWord, String secondWord) {
         int countNumberOfPermutations = 0;
         for(int i = 1; i < firstWord.length(); i++) {
             if (firstWord.charAt(i) != secondWord.charAt(i)) {
@@ -15,6 +24,21 @@ class Utils {
         }
         float twoThirdsOfPermutations = (float) 2 / 3 * firstWord.length();
         return countNumberOfPermutations < twoThirdsOfPermutations;
+    }
+
+    private static boolean checkEachLetter(String firstWord, String secondWord) {
+        for(Character character: firstWord.toCharArray()) {
+            int occurrencesInFirstWord = occurrencesIn(firstWord, character);
+            int occurrencesInSecondWord = occurrencesIn(secondWord, character);
+
+            if (occurrencesInFirstWord != occurrencesInSecondWord) return false;
+        }
+
+        return true;
+    }
+
+    private static int occurrencesIn(String word, Character character) {
+        return word.replaceAll("[^" + character + "]", "").length();
     }
 
 }
